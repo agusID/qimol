ffd0ccc51197e786fe19a5eb66ea0883a85ed677
Dwi Agustianto <dwi.agustianto@ralali.com>
2019-07-24 23:49:10 +0700

    add style

--- src/components/quiz/Quiz.svelte
+++ src/components/quiz/Quiz.svelte
@@ -3,19 +3,19 @@
   import { database } from '@config/firebase'
 
   let temp = []
-  let totalQuestion = 5
+  let totalQuestion = '?'
   let usersRef = database.ref('question')
   let answerChecked = [false, false, false, false]
   let uniqueID = $lastKey == null ?  Math.random().toString(36).substring(7) : $lastKey
   lastKey.update(value => value = uniqueID)
 
   // check username is exists
-  if($username == null){
-    $username = prompt('Input your name : ')
-    while($username == null || $username.length == 0){
-      $username = prompt('Input your name : ')
-    }
-  }
+  // if($username == null){
+  //   $username = prompt('Input your name : ')
+  //   while($username == null || $username.length == 0){
+  //     $username = prompt('Input your name : ')
+  //   }
+  // }
 
   const MAX_SCORE = 100
 
@@ -117,53 +117,269 @@
 
 </script>
 <style>
+  .loader-container {
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .loader {
+    position: relative;
+    width: 80px;
+    height: 80px;
+    display: inline-flex;
+    background-color: transparent;
+    border: 5px solid transparent;
+    border-radius: 50%;
+  }
+
+  .loader:after {
+    position: absolute;
+    content: "";
+    top: 0px;
+    left: 0px;
+    width: 80px;
+    height: 80px;
+    padding: 3px;
+    border-width: 3px;
+    border-style: solid;
+    border-color: #107eeb transparent transparent transparent;
+    border-radius: 50%;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    animation: rotation .8s infinite ease-in-out;
+    -webkit-animation: rotation .8s infinite ease-in-out;
+  }
+
+  .loader-label {
+    margin-top: 50px;
+    color: #ecf0f1;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotateZ(0deg);
+      -webkit-transform: rotateZ(0deg);
+    }
+    100% {
+      transform: rotateZ(360deg);
+      -webkit-transform: rotateZ(360deg);
+    }
+  }
+
+  @-webkit-keyframes rotation {
+    0% {
+      transform: rotateZ(0deg);
+      -webkit-transform: rotateZ(0deg);
+    }
+    100% {
+      transform: rotateZ(360deg);
+      -webkit-transform: rotateZ(360deg);
+    }
+  }
+
+  .container {
+    background-color: #252c4a;
+    height: 100vh;
+    color: white;
+    width: 100%;
+    overflow-x: hidden;
+    font-family: 'Roboto';
+    -webkit-tap-highlight-color: transparent;
+    -moz-tap-highlight-color: transparent;
+  }
+
   .panel {
-    background-color: white;
-    border: 1px solid black;
+    background-color: #252c4a;
     width: 100%;
   }
+
+  .question-indicator {
+    margin: 20px 40px;
+    color: #bdc3c7;
+    font-size: 14px;
+  }
+
+  .question-indicator strong{
+    font-size: 20px;
+  }
+
+  .question {
+    margin: 20px 40px;
+    font-weight: bold;
+    font-size: 18px;
+    min-height: 20vh;
+  }
+
+  .answer-list {
+    margin: 20px;
+  }
+
+  .answer-item {
+    margin: 15px;
+    display: flex;
+    align-items: center;
+  }
+
   .disabled {
     pointer-events: none;
-    opacity: 0.5;
+    opacity: 0.4;
+  }
+
+  .btn-group {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    bottom: 0;
+    background-color: transparent;
+    padding: 10px;
+  }
+
+  .btn {
+    background-color: #107eeb;
+    color: white;
+    border: none;
+    outline: none;
+    font-size: 20px;
+    padding: 10px 15px;
+    border-radius: 20px;
+    margin: 10px;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+  }
+
+  .btn-next, .btn-submit {
+    min-width: 50%;
+  }
+
+  .btn-submit {
+    display: none;
+  }
+
+  .btn-prev {
+    background-color: white;
+    color: black;
+    border-radius: 50%;
+    padding: 10px;
+    margin-right: 0px;
+    font-weight: bold;
+    width: 41px;
+  }
+
+  .answer-label {
+    display: block;
+  }
+  
+  .not-visible {
+    display: none;
+  }
+
+  .visible {
+    display: inline-block;
+  }
+
+  [type="radio"]:checked,
+  [type="radio"]:not(:checked) {
+    position: absolute;
+    left: -9999px;
+  }
+  [type="radio"]:checked + label,
+  [type="radio"]:not(:checked) + label {
+    position: relative;
+    padding: 10px;
+    padding-right: 28px;
+    padding-left: 15px;
+    cursor: pointer;
+    display: block;
+    width: 100%;
+    border: 2px solid #224263;
+    border-radius: 15px;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+  }
+  [type="radio"]:not(:checked) + label {
+    color: #bdc3c7;
+  }
+  [type="radio"]:checked + label {
+    background-color:rgb(18, 125, 233, 0.2);
+    color: white;
+  }
+  [type="radio"]:checked + label:before,
+  [type="radio"]:not(:checked) + label:before {
+    content: '';
+    position: absolute;
+    right: 9px;
+    top: 9px;
+    width: 18px;
+    height: 18px;
+    border: 2px solid #224263;
+    border-radius: 100%;
+    background: transparent;
+  }
+
+  [type="radio"]:checked + label:after,
+  [type="radio"]:not(:checked) + label:after {
+    content: '';
+    right: 9px;
+    top: 9px;
+    width: 18px;
+    height: 18px;
+    background: #107eeb;
+    border: 2px solid #107eeb;
+    position: absolute;
+    border-radius: 100%;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+  }
+
+  [type="radio"]:not(:checked) + label:after {
+    opacity: 0;
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+
+  [type="radio"]:checked + label:after {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
   }
 </style>
-<h1>Quiz {currQuestion + 1}/{totalQuestion}</h1>
-<div class="panel">
-  {#if temp.length > 0}
-    <div class="question-number">#{currQuestion+1}</div>
-    <div class="question">{temp[currQuestion].question}</div>
-    <div class="answer-list">
-      <div class="answer-item">
-        <label>
-          <input type="radio" checked={answerChecked[0]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 1)} name="q-{currQuestion}-answer" />
-          {temp[currQuestion].answer_1}
-        </label>
+<div class="container">
+  <div class="panel">
+    {#if temp.length > 0}
+      <div class="question-indicator"><strong>Question {currQuestion + 1}</strong>/{totalQuestion}</div>
+      <div class="question">{temp[currQuestion].question}</div>
+      <div class="answer-list">
+        <div class="answer-item">
+          <input id="A" type="radio" checked={answerChecked[0]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 1)} name="q-{currQuestion}-answer" />
+          <label class="answer-label" for="A">{temp[currQuestion].answer_1}</label>
+        </div>
+        <div class="answer-item">
+          <input id="B" type="radio" checked={answerChecked[1]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 2)} name="q-{currQuestion}-answer" />
+          <label class="answer-label" for="B">{temp[currQuestion].answer_2}</label>
+        </div>
+        <div class="answer-item">
+          <input id="C" type="radio" checked={answerChecked[2]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 3)} name="q-{currQuestion}-answer" />
+          <label class="answer-label" for="C">{temp[currQuestion].answer_3}</label>
+        </div>
+        <div class="answer-item">
+          <input id="D" type="radio" checked={answerChecked[3]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 4)} name="q-{currQuestion}-answer" />
+          <label class="answer-label" for="D">{temp[currQuestion].answer_4}</label>
+        </div>
       </div>
-      <div class="answer-item">
-        <label>
-          <input type="radio" checked={answerChecked[1]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 2)} name="q-{currQuestion}-answer" />
-          {temp[currQuestion].answer_2}
-        </label>
+      <div class="btn-group">
+        <button class="btn btn-prev" class:disabled="{currQuestion === 0}" on:click={btnPrev}>&LeftArrow;</button>
+        <button class="btn btn-next" class:not-visible="{currQuestion === totalQuestion - 1}" on:click={btnNext}>Next</button>
+        <button class="btn btn-submit" class:visible="{currQuestion >= totalQuestion - 1}" on:click={btnSubmit}>Submit</button>
       </div>
-      <div class="answer-item">
-        <label>
-          <input type="radio" checked={answerChecked[2]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 3)} name="q-{currQuestion}-answer" />
-          {temp[currQuestion].answer_3}
-        </label>
+    {:else}
+      <div class="loader-container">
+        <div class="loader"></div>
+        <div class="loader-label">Getting the Questions</div>
       </div>
-      <div class="answer-item">
-        <label>
-          <input type="radio" checked={answerChecked[3]} on:click={() => doAnswer(currQuestion, temp[currQuestion].unique_id, 4)} name="q-{currQuestion}-answer" />
-          {temp[currQuestion].answer_4}
-        </label>
-      </div>
-    </div>
-  {:else}
-    <div>Please wait...</div>
-  {/if}
-  <div class="button-group">
-    <button class:disabled="{currQuestion === 0}" on:click={btnPrev}>Prev</button>
-    <button class:disabled="{currQuestion === totalQuestion - 1}" on:click={btnNext}>Next</button>
-    <button class:disabled="{currQuestion < totalQuestion - 1}" on:click={btnSubmit}>Submit</button>
+    {/if}
   </div>
 </div>
